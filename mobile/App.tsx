29ed3701821acdb8954bd2e25e66386e188ea59a
xenia <xenia_onescu_3@hotmail.com>
2022-11-18 17:31:01 +0100

    create components

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,41 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StatusBar } from 'react-native';
+import { useFonts, Inter_400Regular,Inter_600SemiBold, Inter_700Bold, Inter_900Black} from '@expo-google-fonts/inter';
+import { Home } from './src/screens/Home';
+import { Loading } from './src/components/Loading';
+import { Background } from './src/components/Background';  
 
-interface ButtonProps {
-  title: string;
-  }
-
-  
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold, 
+    Inter_700Bold, 
+    Inter_900Black
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native </Text>
-      <Button title="Send message 1" />
-      <Button title="Send message 2" />
-      <Button title="Send message 3" />
-      <StatusBar style="auto" backgroundColor='pink'/>
-    </View>
+    <Background>
+     <StatusBar 
+     barStyle="light-content"
+     backgroundColor="transparent"
+     translucent
+     />
+     {fontsLoaded ? <Home /> : <Loading />}
+    </Background>
   );
 };
-
-
-function Button(props: ButtonProps) {
-  return (
-    <TouchableOpacity>
-      <Text>
-  {props.title}
-      </Text>
-    </TouchableOpacity>
-  )
-};
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  
-});
---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,19 @@
+import { View, Image } from 'react-native';
+import  logoImg  from '../../assets/logo-nlw-esports.png';
+import { Heading } from '../../components/Heading';
+import { styles } from './styles';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+     <Image 
+     source={logoImg}
+     style={styles.logo}
+     />
+     <Heading 
+     title="Encontre seu duo!"
+     subtitle="Selecione o game que deseja jogar..."
+     />
+    </View>
+  );
+}
