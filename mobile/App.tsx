2ba5d430fa6086399b129f4cba80aab76d7f004b
xenia <xenia_onescu_3@hotmail.com>
2022-11-18 15:24:10 +0100

    adding folder src and assets

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,14 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
+interface ButtonProps {
+  title: string;
+  }
+
+  
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello React Native </Text>
+      <Button title="Send message 1" />
+      <Button title="Send message 2" />
+      <Button title="Send message 3" />
+      <StatusBar style="auto" backgroundColor='pink'/>
     </View>
   );
-}
+};
+
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>
+  {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
+};
+
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +37,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
 });
