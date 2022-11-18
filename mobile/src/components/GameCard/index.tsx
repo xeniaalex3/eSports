46ab28669c6d767bf5fb25a66294ee222b0d6821
xenia <xenia_onescu_3@hotmail.com>
2022-11-18 18:25:08 +0100

    create GameCard component

--- mobile/src/components/GameCard/index.tsx
+++ mobile/src/components/GameCard/index.tsx
@@ -1,11 +1,39 @@
-import { View } from 'react-native';
-
+import { TouchableOpacity, TouchableOpacityProps, ImageBackground, ImageSourcePropType, Text } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 import { styles } from './styles';
+import { THEME } from '../../theme';
 
-export function GameCard() {
+export interface GameCardProps {
+  id: string;
+  name: string;
+  ads: string;
+  cover: ImageSourcePropType;
+}
+
+interface Props extends TouchableOpacityProps {
+  data: GameCardProps;
+}
+
+export function GameCard({data, ...rest}: Props) {
   return (
-    <View style={styles.container}>
+    <TouchableOpacity style={styles.container} {...rest}>
+      <ImageBackground 
+      style={styles.cover}
+      source={data.cover}
+      />
 
-    </View>
+      <LinearGradient 
+      colors={THEME.COLORS.FOOTER}
+      style={styles.footer}
+      />
+
+      <Text style={styles.name}>
+      {data.name}
+      </Text> 
+     
+     <Text style={styles.ads}>
+      {data.ads}
+     </Text>
+    </TouchableOpacity>
   );
 }
--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,6 +1,8 @@
 import { View, Image } from 'react-native';
 import  logoImg  from '../../assets/logo-nlw-esports.png';
+import { GameCard } from '../../components/GameCard';
 import { Heading } from '../../components/Heading';
+import { GAMES } from '../../utils/games';
 import { styles } from './styles';
 
 export function Home() {
@@ -13,6 +15,10 @@ export function Home() {
      <Heading 
      title="Encontre seu duo!"
      subtitle="Selecione o game que deseja jogar..."
+     />
+
+     <GameCard 
+     data={GAMES[0]}
      />
     </View>
   );
