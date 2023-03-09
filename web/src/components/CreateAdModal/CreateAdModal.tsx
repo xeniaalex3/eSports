3bfc57135eea5e9ca0aebff54894ac27a70c19be
xenia <xenia_onescu_3@hotmail.com>
2023-03-09 13:04:00 +0100

    debug tailwind

--- web/src/components/CreateAdModal/CreateAdModal.tsx
+++ web/src/components/CreateAdModal/CreateAdModal.tsx
@@ -29,13 +29,13 @@ export function CreateAdModal() {
     <Dialog.Portal>
       <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
       <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25">
-        <Dialog.Title className="text-3xl font-black">
+        <Dialog.Title className="text-2xl font-black">
         Publish an ad
         </Dialog.Title>
 
         <form className='mt-8 flex flex-col gap-4'>
           <div className="flex flex-col gap-2">
-            <label htmlFor="game" className="font-semibold">Which game?</label>
+            <label htmlFor="game" className='text-base'>Which game?</label>
             <select 
             id='game' 
             className='bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500'
@@ -51,18 +51,18 @@ export function CreateAdModal() {
           </div>
 
           <div className='flex flex-col gap-2 mt-3'>
-            <label htmlFor="name" className="font-semibold">Your name (or nickname)</label>
+            <label htmlFor="name" className='text-base'>Your name (or nickname)</label>
             <Input id='name' placeholder='Como te chamam dentro do game?' />
           </div>
 
           <div className='grid grid-cols-2 gap-6'>
             <div className='flex flex-col gap-2'>
-              <label htmlFor="yearsPlaying" className='font-semibold'>How many years have you been playing?</label>
+              <label htmlFor="yearsPlaying" className='text-base'>How many years have you been playing?</label>
               <Input id='yearsPlaying' placeholder='Tudo bem ser ZERO' type="number" />
             </div>
 
-            <div className='flex flex-col gap-2'>
-              <label htmlFor="discord">What is your Discord?</label>
+            <div className='flex flex-col gap-8'>
+              <label htmlFor="discord" className='text-base'>What is your Discord?</label>
               <Input id='discord' placeholder='Usuario#0000' type="text" />
             </div>
           </div>
