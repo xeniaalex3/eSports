e750e4d0cf2716113f59881992aa28926b1cf5e8
xenia <xenia_onescu_3@hotmail.com>
2023-03-14 18:01:17 +0100

    add new libraries notifications

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,18 +1,31 @@
+import { useRef, useEffect } from 'react';
 import { StatusBar } from 'react-native';
 import { useFonts, Inter_400Regular,Inter_600SemiBold, Inter_700Bold, Inter_900Black} from '@expo-google-fonts/inter';
+import { Subscription } from 'expo-modules-core';
 import { Routes } from './src/routes';
 import { Loading } from './src/components/Loading';
 import { Background } from './src/components/Background';  
 
-export default function App() {
+import './src/services/notificationConfig';
+import { getPushNotificationToken } from './src/services/getPushNotificationToken';
 
-  const [fontsLoaded] = useFonts({
+
+
+export default function App() {
+const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_600SemiBold, 
     Inter_700Bold, 
     Inter_900Black
   });
+  const getNotificationListener = useRef<Subscription>();
+  const responseNotificationListener = useRef<Subscription>();
+  
+  useEffect(() => {
+    getPushNotificationToken();
+  })
 
+  
   return (
     <Background>
      <StatusBar 
